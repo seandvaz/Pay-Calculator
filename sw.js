92d613f924221132caf233983c19fa524ac9e84e
seandvaz <312807467+seandvaz@users.noreply.github.com>
2026-08-04 12:14:47 +0800

    add web app files

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='pta-pay-v2';const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
